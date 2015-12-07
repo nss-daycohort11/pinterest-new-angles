da9dca339fe5bfaf738eb5c059cc820b18aa7c36
Stephanie Miranda <stephanieMiranda@Stephanies-MacBook-Pro.local>
2015-12-07 17:53:08 -0600

    deleted unsued file

--- app/controllers/add.js
+++
@@ -1,9 +0,0 @@
-app.controller("addCtrl", ["$scope", "$firebaseAuth","getUid", function ($scope, $firebaseAuth, getUid) {
-
-	console.log("app controller is linked");
-
-	$scope.addNew = function (){
-
-	};
-
-}]); //end of contoller
