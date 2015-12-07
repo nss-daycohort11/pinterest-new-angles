4250844cafc7b8856f1381b51ddc7b2086c86156
Stephanie Miranda <stephanieMiranda@Stephanies-MacBook-Pro.local>
2015-12-07 14:03:27 -0600

    facebook auth works

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,10 +1,20 @@
-app.controller("loginCtrl", ["$scope", function ($scope) {
+app.controller("loginCtrl", ["$scope", "$firebaseAuth", function ($scope, $firebaseAuth) {
 
-		console.log("contoller is linked");
+	console.log("contoller is linked");
 
-	$scope.login = function () {
-		console.log("login function");
-	};	
+  	//login
+  	$scope.login = function () {
+		var ref = new Firebase("https://newangles.firebaseio.com");
+
+	 	// create an instance of the authentication service
+	  	var auth = $firebaseAuth(ref);
+  		auth.$authWithOAuthPopup("facebook").then(function (authData){
+  			console.log("logged in as:", authData.uid);
+  		}).catch(function(error){
+  			console.log("authentication failed:", error);
+  		});
+  	};
+
 
 
 
